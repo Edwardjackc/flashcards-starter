268d138b93b014bee112b2852b9f61354f1458d1
Edward Cheatham <edwardjackc@gmail.com>
2019-06-19 13:00:48 -0600

    First Commit

---
+++ test/Game-test.js
@@ -0,0 +1,26 @@
+const chai = require("chai");
+const expect = chai.expect;
+const Game = require("../src/Game");
+const Deck = require("../src/Deck");
+const Card = require("../src/Card");
+const Round = require("../src/Round");
+
+describe('Game', function () {
+
+  it.skip('should be a function', function () {
+    expect(Game).to.be.a('function');
+  });
+
+  it.skip('should be an instance of Game', function () {
+    const game = new Game();
+    expect(game).to.be.an.instanceof(Game);
+  });
+
+  it.skip('should start a new game', function () {
+    const game = new Game();
+    game.start()
+    let round = game.currentRound;
+    expect(round.turns).to.equal(0);
+    expect(round.incorrectGuesses).to.eql([]);
+  })
+});
